b63057922403bb42c9775bd6150f8d3aa6bafc6c
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-03-16 03:11:25 +0300

    555
    555

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Follow
 
+
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('user', 'following')
-
+    list_display = ("user", "following")
