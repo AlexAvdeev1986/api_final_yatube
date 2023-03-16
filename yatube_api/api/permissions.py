9b591a55255edae4dd3f0e04926bd253d38746e1
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-03-16 21:32:04 +0300

    555
    555

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,14 +2,12 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
         """Чтение все. Измнения только авторизованные пользователи"""
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        elif request.user.is_authenticated:
-            return True
-        return False
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         """Чтение все. Изменения только авторы."""
