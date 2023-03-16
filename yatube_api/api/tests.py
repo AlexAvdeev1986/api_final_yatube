d65c0d5937b044040545f8269cb234ef6360efd5
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-03-16 03:21:59 +0300

    888
    888

--- yatube_api/api/tests.py
+++ yatube_api/api/tests.py
@@ -1,3 +1,4 @@
+from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -5,7 +6,7 @@ from django.contrib.auth import get_user_model
 from posts.models import Follow
 
 
-class FollowTests():
+class FollowTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user1 = get_user_model().objects.create_user(
